c2501800548f5f7d8c03966ab59ba9006f074008
websevak7-max <websevak7@gmail.com>
2026-07-15 02:16:22 -0700

    1st

--- ucs crm/src/panels/hr/components/forms/Template1.jsx
+++ ucs crm/src/panels/hr/components/forms/Template1.jsx
@@ -112,7 +112,7 @@ export default function Template1({ personal, education }) {
           </tr>
         </table>
         <table>
-          <tr><td colSpan="5" className="section">FAMILY DETAILS</td></tr>
+          <tr><td colSpan="5" className="section">FAMILY DETAILS/Referernce</td></tr>
           <tr><th width="8%">S.No</th><th>Name</th><th>Relation</th><th>Occupation</th><th>Date of Birth</th></tr>
           {[...Array(3)].map((_, i) => (
             <tr key={i}>
